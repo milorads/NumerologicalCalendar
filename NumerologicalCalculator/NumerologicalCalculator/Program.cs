16a7c825937b0a73b4c4a2ddf27e9d8e9b13cfac
Milorad Sekulovic <milorad.sekulovic@gmail.com>
2017-03-14 20:03:42 +0100

    added datetime readers and parsers

--- NumerologicalCalculator/NumerologicalCalculator/Program.cs
+++ NumerologicalCalculator/NumerologicalCalculator/Program.cs
@@ -8,10 +8,40 @@ namespace NumerologicalCalculator
 {
     class Program
     {
+        private static DateTime startDate;
+        private static DateTime endDate;
         static void Main(string[] args)
         {
             Console.WriteLine("Write start date(format - dd.mm.yyyy)");
-            var startDate = Console.ReadLine();
+            var startDateInput = Console.ReadLine();
+            CheckInput(startDateInput, true);
+            Console.WriteLine("Write end date(format - dd.mm.yyyy)");
+            var endDateInput = Console.ReadLine();
+            CheckInput(endDateInput, false);
+            for (DateTime date = startDate ; date <= endDate; date += TimeSpan.FromDays(1))
+            {
+                Calculate(date);
+            }
+        }
+
+        private static void Calculate(DateTime date)
+        {
+        }
+
+        private static void CheckInput(string input, bool startDate)
+        {
+            DateTime outDate;
+            DateTime.TryParse(input, out outDate);
+            if (startDate)
+            {
+                Console.WriteLine("Starting date set to: " + outDate.ToLongDateString());
+                startDate = outDate;
+            }
+            else
+            {
+                Console.WriteLine("Ending date set to: " + outDate.ToLongDateString());
+                endDate = outDate;
+            }
         }
     }
 }
