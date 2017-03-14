b6e97c6aaee4a80ba7b7c22a512e6afa4b5c4aff
Milorad Sekulovic <milorad.sekulovic@gmail.com>
2017-03-14 21:30:53 +0100

    finished calculations

--- NumerologicalCalculator/NumerologicalCalculator/Program.cs
+++ NumerologicalCalculator/NumerologicalCalculator/Program.cs
@@ -10,6 +10,8 @@ namespace NumerologicalCalculator
     {
         private static DateTime startDate;
         private static DateTime endDate;
+        private static Dictionary<DateTime, int> listOfCalculatedDates;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Write start date(format - dd.mm.yyyy)");
@@ -19,11 +21,37 @@ namespace NumerologicalCalculator
             var endDateInput = Console.ReadLine();
             CheckInput(endDateInput, false);
             Calculate();
+            Console.WriteLine("Please write desired value 1-9 or 11 or 22 (!0 to list all the values)");
+            var desiredNumberInput = Console.ReadLine();
+            var desiredNumber = int.Parse(desiredNumberInput);
+            if (desiredNumber > 0 && desiredNumber <= 9 || desiredNumber == 11 || desiredNumber == 22)
+            {
+                foreach (var dateIntPair in listOfCalculatedDates)
+                {
+                    if (dateIntPair.Value ==desiredNumber)
+                    {
+                        Console.WriteLine(dateIntPair.Key.ToLongDateString());
+                    }
+                }
+            }
+            else if (desiredNumber == 0)
+            {
+                foreach (var dateIntPair in listOfCalculatedDates)
+                {
+                        Console.Write(dateIntPair.Key.ToLongDateString());
+                    Console.WriteLine(" - This day has value: "+ dateIntPair.Value.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong number input");
+            }
+            Console.Read();
         }
 
         private static void Calculate()
         {
-            Dictionary<DateTime, int> listOfCalculatedDates = new Dictionary<DateTime, int>();
+            listOfCalculatedDates = new Dictionary<DateTime, int>();
             for (DateTime date = startDate; date <= endDate; date += TimeSpan.FromDays(1))
             {
                 var sum = 0;
@@ -45,18 +73,42 @@ namespace NumerologicalCalculator
                     sum += year%10;
                     year /= 10;
                 }
+                var sumOfSum = 0;
                 if (checkIfSpecial(sum))
                 {
                     listOfCalculatedDates.Add(date,sum);
                 }
                 else
                 {
-                    while (true)
+                    while (sum != 0)
                     {
-                        
+                        if (checkIfSpecial(sum))
+                        {
+                            listOfCalculatedDates.Add(date, sum);
+                        }
+                        else
+                        {
+                            sumOfSum += sum % 10;
+                            sum /= 10;
+                        }
+                    }
+                    if (sumOfSum/10 >0)
+                    {
+                        sumOfSum = sumOfSum / 10 + sumOfSum % 10;
+                        listOfCalculatedDates.Add(date,sumOfSum);
+                    }
+                    else
+                    {
+                        listOfCalculatedDates.Add(date,sumOfSum);
+
                     }
                 }
             }
+        }
+
+        public static double sumDigits(double n)
+        {
+            return (1 + ((n - 1) % 9));
         }
 
         private static bool checkIfSpecial(int input)
