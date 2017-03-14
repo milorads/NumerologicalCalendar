9e2b8fdc0e2b549071a0f4155a0cc25b9565e2df
Milorad Sekulovic <milorad.sekulovic@gmail.com>
2017-03-14 20:59:30 +0100

    adding calculation of date's number

--- NumerologicalCalculator/NumerologicalCalculator/Program.cs
+++ NumerologicalCalculator/NumerologicalCalculator/Program.cs
@@ -18,21 +18,64 @@ namespace NumerologicalCalculator
             Console.WriteLine("Write end date(format - dd.mm.yyyy)");
             var endDateInput = Console.ReadLine();
             CheckInput(endDateInput, false);
-            for (DateTime date = startDate ; date <= endDate; date += TimeSpan.FromDays(1))
+            Calculate();
+        }
+
+        private static void Calculate()
+        {
+            Dictionary<DateTime, int> listOfCalculatedDates = new Dictionary<DateTime, int>();
+            for (DateTime date = startDate; date <= endDate; date += TimeSpan.FromDays(1))
             {
-                Calculate(date);
+                var sum = 0;
+                var day = date.Day;
+                var month = date.Month;
+                var year = date.Year;
+                while (day != 0)
+                {
+                    sum += day % 10;
+                    day /= 10;
+                }
+                while (month != 0)
+                {
+                    sum += month % 10;
+                    month /= 10;
+                }
+                while (year !=0)
+                {
+                    sum += year%10;
+                    year /= 10;
+                }
+                if (checkIfSpecial(sum))
+                {
+                    listOfCalculatedDates.Add(date,sum);
+                }
+                else
+                {
+                    while (true)
+                    {
+                        
+                    }
+                }
             }
         }
 
-        private static void Calculate(DateTime date)
+        private static bool checkIfSpecial(int input)
         {
+            if (input == 11 || input == 22)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        private static void CheckInput(string input, bool startDate)
+        private static void CheckInput(string input, bool sd)
         {
             DateTime outDate;
             DateTime.TryParse(input, out outDate);
-            if (startDate)
+            if (sd)
             {
                 Console.WriteLine("Starting date set to: " + outDate.ToLongDateString());
                 startDate = outDate;
