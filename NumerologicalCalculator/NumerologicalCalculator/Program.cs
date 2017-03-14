041778bd79aa69288aeb9286f675c3a51adfb153
Milorad Sekulovic <milorad.sekulovic@gmail.com>
2017-03-14 19:49:56 +0100

    initial commit

---
+++ NumerologicalCalculator/NumerologicalCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerologicalCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write start date(format - dd.mm.yyyy)");
+            var startDate = Console.ReadLine();
+        }
+    }
+}
